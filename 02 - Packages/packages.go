265b54ce69e02d7bdeef2456ea0231ea8b73c4b1
jpwebdev <jjpwebdev@gmail.com>
2018-09-09 11:28:11 -0400

    Functions continued and comments added

--- 02 - Packages/packages.go
+++ 02 - Packages/packages.go
@@ -6,6 +6,9 @@ import (
 	"math/rand"
 )
 
+// Notice how Intn imported from rand stards with capital 'I'
+// Packages only export capitalized methods.
+
 func main() {
 	fmt.Println("My favorite number is:", rand.Intn(10))
 	fmt.Printf("The number %g is my favorite number\n", math.Sqrt(49))
