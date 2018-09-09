66d8a7a5a12477c16545283d57afcfafdcbca166
jpwebdev <jjpwebdev@gmail.com>
2018-09-09 11:27:41 -0400

    Functions continued and comments added

--- 03 - Functions/functions.go
+++ 03 - Functions/functions.go
@@ -6,6 +6,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+// When two or more args share the same type
+// we can omit the type on all except the last
+func addMultiFive(a, b, c, d, e int) int {
+	return a + b + c + d + e
+}
+
 func main() {
 	fmt.Println(add(42, 11))
+	fmt.Println(addMultiFive(7, 9, 1, 0, 10))
 }
